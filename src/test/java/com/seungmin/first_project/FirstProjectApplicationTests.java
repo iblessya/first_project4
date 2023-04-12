afd019f5b25270af941ee221091082163f245fc0
Seungmin Yu <seungmin.yu.7@gmail.com>
2023-04-12 12:25:54 +0900

    Initial commit

---
+++ src/test/java/com/seungmin/first_project/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungmin.first_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
